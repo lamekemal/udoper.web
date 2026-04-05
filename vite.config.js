248fd4ed53da023607fe7b8bc3ce68eeedd700d2
lamekemal <handil@live.fr>
2026-04-05 12:03:38 +0000

    Add console and web routes, implement settings management, and enhance testing suite
    - Created console command for inspiring quotes.
    - Added settings routes for user profile, appearance, and security management.
    - Implemented web routes for home and dashboard views.
    - Established .gitignore files for various storage directories to manage ignored files.
    - Developed comprehensive feature tests for authentication, email verification, password reset, registration, and dashboard access.
    - Introduced Livewire components for profile and security settings with corresponding tests.
    - Configured Vite for asset management with Tailwind CSS support.

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import {
+    defineConfig
+} from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        host: '192.168.100.214', // or your specific local IP
+        cors: true,
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
