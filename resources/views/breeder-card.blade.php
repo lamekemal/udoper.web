777375f2e4f56adad046ed47088046ce9c08417b
lamekemal <handill@live.fr>
2026-05-19 21:31:09 +0100

    xdzef

--- resources/views/breeder-card.blade.php
+++ resources/views/breeder-card.blade.php
@@ -84,8 +84,8 @@
 
         /* Style unique pour les deux logos */
         .card-logo {
-            width: 70px;
-            height: 70px;
+            width: 40px;
+            height: 40px;
             object-fit: contain;      /* Évite de déformer vos logos s'ils ne sont pas parfaitement carrés */
         }
 
